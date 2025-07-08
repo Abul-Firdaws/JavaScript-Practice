23f58a2e14dd41648e33f1f5f4d9923d9d9e8658
Saaka Abdul-Firdaws <saakaabufirdaus@gmail.com>
2025-07-08 18:16:48 +0000

    Added JavaScript summary using Vim and Node.js

---
+++ summary.js
@@ -0,0 +1,11 @@
+const editor = "vim";
+const isPowerful = true;
+const isEasy = false;
+
+console.log("Welcome to my JavaScript Terminal Summary!");
+console.log('I used Vim to create and edit this file.');
+console.log("Is Vim powerful? " + isPowerful);
+console.log("Is Vim easy for beginners? " + isEasy);
+
+let message = "Learning with Node.js is fun!";
+console.log(message)
